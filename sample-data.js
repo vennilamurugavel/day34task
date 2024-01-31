8e4c8e2180e5cb95fbb16037cafd6d5bf9578013
vennilamurugavel <vennilageo@gmail.com>
2024-01-31 18:44:53 +0530

    first commit

---
+++ sample-data.js
@@ -0,0 +1,32 @@
+let rooms = [{
+    roomId:"R1",
+    seatsAvailable:"4",
+    amenities:"tv,ac,heater",
+    pricePerhr:"100"
+}];
+let bookings = [{
+    customer: "vennila",
+    bookingDate: "20230612",
+    startTime: "12:00pm",
+    endTime: "11:59am",
+    bookingID: "B1",
+    roomId: "R1",
+    status: "booked",
+    booked_On: "3/1/2024"
+}
+];
+let customers = [
+    { name: 'vennila',
+     bookings: [ 
+        {
+            customer: 'vennila',
+            bookingDate: '20230612',
+            startTime: '12:00pm',
+            endTime: '11:59am',
+            bookingID: 'B1',
+            roomId: 'R1',
+            status: 'booked',
+            booked_On: '3/1/2024'
+          }
+      ] }
+];
